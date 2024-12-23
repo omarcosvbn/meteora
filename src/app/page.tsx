6706293aecc6d30355915d275a8c08b712861859
omarcosvbn <omarcosvbn@gmail.com>
2024-12-23 10:00:41 +0000

    modal almost done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,6 @@ export default function Home() {
     e.preventDefault(); // Prevent form from refreshing the page
     setNewsletterModal(true); // Open the newsletter modal
   };
-  
 
   return (
     <>
@@ -229,7 +228,10 @@ export default function Home() {
               Cadastre-se!
             </strong>
           </h5>
-          <form className={styles.newsletter__form} onSubmit={handleNewsletterSubmit}>
+          <form
+            className={styles.newsletter__form}
+            onSubmit={handleNewsletterSubmit}
+          >
             <input
               type="email"
               placeholder="Digite seu email"
@@ -245,11 +247,168 @@ export default function Home() {
         </div>
       </div>
 
-      <Modal title="E-mail cadastrado com sucesso!" openModal={newsletterModal} closeModal={() => setNewsletterModal(false)}>
+      <Modal
+        title="E-mail cadastrado com sucesso!"
+        openModal={newsletterModal}
+        closeModal={() => setNewsletterModal(false)}
+      >
         <p>Em breve você receberá novidades exclusivas da Meteora.</p>
       </Modal>
-      <Modal title="E-mail cadastrado com sucesso!" openModal={productModal} closeModal={() => setProductModal(false)}>
-        <p>Em breve você receberá novidades exclusivas da Meteora.</p>
+
+      <Modal
+        title="Confira detalhes sobre o produto"
+        openModal={productModal}
+        closeModal={() => setProductModal(false)}
+      >
+        <div className={styles.details}>
+          <picture>
+            <source
+              media="(max-width: 768px)"
+              srcSet="/Desktop/Imagens Cards/Jaqueta1-2.png"
+            />
+            <Image
+              src="/Desktop/Imagens Cards/Jaqueta1.png"
+              alt="Jaqueta"
+              width={350}
+              height={422}
+              className={styles.details__image}
+            />
+          </picture>
+
+          <div className={styles.details__info}>
+            <div className={styles.details__preform}>
+              <div className={styles.details__first}>
+                <span className={styles.details__first__name}>
+                  Jaqueta Jeans
+                </span>
+                <p className={styles.details__first__description}>
+                  Modelo unissex oversized com gola de camurça. Atemporal e
+                  autêntica!
+                </p>
+              </div>
+              <div className={styles.details__second}>
+                <span className={styles.details__second__price}>R$ 150,00</span>
+                <p className={styles.details__second__seller}>
+                  Vendido e entregue por Riachuelo
+                </p>
+              </div>
+            </div>
+
+            <form className={styles.details__form}>
+              <fieldset className={styles.details__form__field}>
+                <h3>Cores:</h3>
+                <div className={styles.details__form__field__selector}>
+                  <div className={styles.details__form__field__selector__input}>
+                    <input
+                      type="radio"
+                      id="AzulClaro"
+                      name="color"
+                      value="AzulClaro"
+                      className={
+                        styles.details__form__field__selector__input__input
+                      }
+                      defaultChecked
+                    />
+                    <label htmlFor="AzulClaro">Azul claro</label>
+                  </div>
+                  <div className={styles.details__form__field__selector__input}>
+                    <input
+                      type="radio"
+                      id="Offwhite"
+                      name="color"
+                      value="Offwhite"
+                      className={
+                        styles.details__form__field__selector__input__input
+                      }
+                    />
+                    <label htmlFor="Offwhite">Offwhite</label>
+                  </div>
+                  <div className={styles.details__form__field__selector__input}>
+                    <input
+                      type="radio"
+                      id="Preto"
+                      name="color"
+                      value="Preto"
+                      className={
+                        styles.details__form__field__selector__input__input
+                      }
+                    />
+                    <label htmlFor="Preto">Preto</label>
+                  </div>
+                </div>
+              </fieldset>
+              <fieldset className={styles.details__form__field}>
+                <h3>Tamanho:</h3>
+                <div className={styles.details__form__field__selector}>
+                  <div className={styles.details__form__field__selector__input}>
+                    <input
+                      type="radio"
+                      id="P"
+                      name="size"
+                      value="P"
+                      className={
+                        styles.details__form__field__selector__input__input
+                      }
+                      defaultChecked
+                    />
+                    <label htmlFor="P">P</label>
+                  </div>
+                  <div className={styles.details__form__field__selector__input}>
+                    <input
+                      type="radio"
+                      id="PP"
+                      name="size"
+                      value="PP"
+                      className={
+                        styles.details__form__field__selector__input__input
+                      }
+                    />
+                    <label htmlFor="PP">PP</label>
+                  </div>
+                  <div className={styles.details__form__field__selector__input}>
+                    <input
+                      type="radio"
+                      id="M"
+                      name="size"
+                      value="M"
+                      className={
+                        styles.details__form__field__selector__input__input
+                      }
+                    />
+                    <label htmlFor="M">M</label>
+                  </div>
+                  <div className={styles.details__form__field__selector__input}>
+                    <input
+                      type="radio"
+                      id="G"
+                      name="size"
+                      value="G"
+                      className={
+                        styles.details__form__field__selector__input__input
+                      }
+                    />
+                    <label htmlFor="G">G</label>
+                  </div>
+                  <div className={styles.details__form__field__selector__input}>
+                    <input
+                      type="radio"
+                      id="GG"
+                      name="size"
+                      value="GG"
+                      className={
+                        styles.details__form__field__selector__input__input
+                      }
+                    />
+                    <label htmlFor="GG">GG</label>
+                  </div>
+                </div>
+              </fieldset>
+              <button className={styles.details__form__submit} type="submit">
+                Adicionar à sacola
+              </button>
+            </form>
+          </div>
+        </div>
       </Modal>
     </>
   );
